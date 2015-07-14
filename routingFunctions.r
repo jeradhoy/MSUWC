5538c0c620de3dbf9d5a7a62bc14da7f62d0b714
jeradhoy <jeradhoy@gmail.com>
2015-07-13 18:43:15 -0600

    added new Rs routing algorithm

--- routingFunctions.r
+++ routingFunctions.r
@@ -74,12 +74,9 @@ assignContribArea <- function(edges){
     for(i in 1:nrow(edges)){
         
         if(edges[i,]$RiverOrder == 1){
-            #print(paste("Area of stream order", edges[i,]$RiverOrder, "is", edges[i,]$Shape_Ar_1))
             edges[i,"ContribArea"] <- edges[i,]$Shape_Ar_1
         } else {
-            #print(paste("Area of stream order", edges[i,]$RiverOrder, "is", sum(edges[edges$NextDownID == edges[i,]$HydroID_1,]$Shape_Ar_1) + edges[i,]$Shape_Ar_1))
-            #print(sum(edges[edges$NextDownID == edges[i,]$HydroID_1,]$Shape_Ar_1) + edges[i,]$Shape_Ar_1)
-            edges[i,"ContribArea"] <- sum(edges[edges$NextDownID == edges[i,]$HydroID_1,]$ContribArea) + edges[i,]$Shape_Ar_1 #sum(results$qIn[month,c(as.character(parentEdges$HydroID_1))])
+            edges[i,"ContribArea"] <- sum(edges[edges$NextDownID == edges[i,]$HydroID_1,]$ContribArea) + edges[i,]$Shape_Ar_1 
         }
         
     }
@@ -92,7 +89,7 @@ assignBfDepth <- function(edges, a, b){
     edges$bfDepth <- NA
     
     for(i in 1:nrow(edges)){
-        edges[i,"bfDepth"] <- a(edges[i,]$ContribArea)^b
+        edges[i,"bfDepth"] <- a*(edges[i,]$ContribArea)^b
     }
     return(edges)
 }
@@ -103,7 +100,7 @@ assignBfWidth <- function(edges, a, b){
     edges$bfWidth <- NA
     
     for(i in 1:nrow(edges)){
-        edges[i,"bfWidth"] <- a(edges[i,]$ContribArea)^b
+        edges[i,"bfWidth"] <- a*(edges[i,]$ContribArea)^b
     }
     return(edges)
 }
@@ -113,4 +110,71 @@ getCatchInBounds <- function(catchments, hucCodes){
     catchmentCodes[is.na(catchmentCodes)] <- 0
     catchments$HUC10 <- catchmentCodes
     return(catchments[catchments$HUC10 %in% hucCodes,])
+}
+
+
+
+
+routeWaterDimensions <- function(edges, Rs){#, varTable=FALSE, plotNetwork=FALSE, writeData=FALSE){
+    
+    #Order edges by Shreve order so calculation is in right order
+    edges <- edges[order(edges$RiverOrder),]
+    
+    results <- list(
+    qIn = matrix(0, nrow=nrow(Rs), ncol=ncol(Rs),
+        dimnames=list(c(1:nrow(Rs)), edges$HydroID_1)),
+    qOut = matrix(0, nrow=nrow(Rs), ncol=ncol(Rs),
+        dimnames=list(c(1:nrow(Rs)), edges$HydroID_1)),
+    sRiv = matrix(0, nrow=nrow(Rs), ncol=ncol(Rs),
+        dimnames=list(c(1:nrow(Rs)), edges$HydroID_1))
+    )
+    
+    for(day in 1:nrow(Rs)){
+	if(day == 1){
+	    start <- Sys.time()
+	}
+
+        for(i in 1:nrow(edges)){
+
+            hydroID <- edges[i,]$HydroID_1
+            
+            if(edges[i,]$RiverOrder == 1){
+                qIn <- 0
+            } else {
+                qIn <- sum(results$qOut[day,
+                as.character(
+                edges[edges$NextDownID == edges[i,]$HydroID_1,]$HydroID_1)])
+                results$qIn[day, as.character(hydroID)] <- qIn #sum(results$qIn[day,c(as.character(parentEdges$HydroID_1))])
+            }
+
+	    rS <- Rs[day, as.character(hydroID)]
+	    len <- edges$LengthKM[i]
+	    width <- edges$bfWidth[i]
+	    #slope <- edges$Slope[i]
+	    #manningN <- edges$manningN[i]
+
+	    if(day == 1){
+		sRiv <- 0
+		height <- .5 	
+	    } else {
+		sRiv <- results$sRiv[day-1, as.character(hydroID)]
+		height <- (results$qOut[day-1, as.character(hydroID)] * 86400)/(len * width)
+	    }
+	    
+	    #v <- (((height*witdth)/(2*height+width))^(2/3) * (slope)^.5)/manningN 
+
+	    v <- 1.5*86.4 #to convert to km per day
+
+	    qOut <- sRiv + (1-len/v)*qIn + (rS - (len*rS)/(2*v))  #(1-len^2/(2*v))*rS
+	    #qOut <- sRiv + (1-len/v)*(qIn+rS)
+            results$qOut[day, as.character(hydroID)] <- qOut
+            results$sRiv[day, as.character(hydroID)] <- sRiv + rS + qIn - qOut
+            
+         }
+	if(day == 10){
+	    print(paste("ETA:", round((Sys.time()-start)*length((day+1):nrow(Rs))/10, 2), "seconds or", round((Sys.time()-start)*length((day+1):nrow(Rs))/10/60, 2), "minutes. Will finish at", (Sys.time()-start)*length((day+1):nrow(Rs))/10+Sys.time()))
+	}
+    }
+    
+    return(results)
 }
